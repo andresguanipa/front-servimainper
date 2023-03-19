558f9983c6683d7b354f3048c4241349aa490662
Andres <andreseguanipa1@gmail.com>
2023-03-19 19:52:20 -0400

    fixed: react router dom footer

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -25,10 +25,10 @@ const Footer = () => {
             <div className="footer-bottom">
                 <p>
                     <Link className='blogItem-link' to={routes.home}>
-                        <a><b>Home</b></a>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                        <b>Home</b>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                     </Link>
                     <Link className='blogItem-link' to={routes.register}>
-                        <a><b>Register</b></a>
+                        <b>Register</b>
                     </Link>
                 </p>
 
