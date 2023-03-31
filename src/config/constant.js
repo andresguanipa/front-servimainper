0cbf887d06292a9133639ef1593952a68c8fe9dc
Andres <andreseguanipa1@gmail.com>
2023-03-30 20:38:08 -0400

    feat: link production

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,5 +1,5 @@
 //const URL_API = 'http://localhost:5000/api';
-const URL_API = 'https://back-servimainper-production.up.railway.app/api'
+const URL_API = 'https://servimainper-back-production.up.railway.app/api'
 const URL_IMG = 'https://res.cloudinary.com/dlvlxxe5t/image/upload/v1675527840/equaldata/no-image_bsdux3.png';
 
 const types = {
